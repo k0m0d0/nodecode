c0046b53eb916eddac8a0684cb0f46e1611b1e24
k0m0d0 <akakomodo@gmail.com>
2018-06-26 11:10:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ runGetRequest();
 function runGetRequest() {
     
     //sample URL.
-    var url = "http://140.86.15.104:3000/fighters/45/4/green/k0m0d0";
+    var url = "http://140.86.15.104:3000/fighters/45/7/green/k0m0d0";
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
