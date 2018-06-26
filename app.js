5b5457c05d69cc79ce53d1de7f43af6479d8d4dc
k0m0d0 <akakomodo@gmail.com>
2018-06-26 11:17:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function runGetRequest() {
 
 //Executes a SQL query
 function runDatabaseQuery() {
-    connection.query("SELECT * FROM SampleTable", function(error, rows, fields) {
+    connection.query("SELECT * FROM SecretTable", function(error, rows, fields) {
         if(!error) {
             console.log(rows);
         } else {
